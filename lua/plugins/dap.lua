f1ab8a0f0831bc1881636440d0bde8b1f0cc0d59
Redyf <mateusalvespereira7@gmail.com>
2024-02-21 21:49:22 -0300

    feat: Add additional options to rust dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -27,7 +27,46 @@ return {
 					args = {},
 				},
 			}
-			dap.configurations.rust = dap.configurations.cpp
+
+			dap.configurations.rust = {
+				{
+					name = "Launch",
+					type = "lldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+					initCommands = function()
+						-- Find out where to look for the pretty printer Python module
+						local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+
+						local script_import = 'command script import "'
+							.. rustc_sysroot
+							.. '/lib/rustlib/etc/lldb_lookup.py"'
+						local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+						local commands = {}
+						local file = io.open(commands_file, "r")
+						if file then
+							for line in file:lines() do
+								table.insert(commands, line)
+							end
+							file:close()
+						end
+						table.insert(commands, 1, script_import)
+
+						return commands
+					end,
+					name = "Attach to process",
+					type = "rust",
+					request = "attach",
+					pid = require("dap.utils").pick_process,
+					args = {},
+				},
+			}
 		end,
 	},
 	vim.keymap.set("n", "<leader>dB", function()
