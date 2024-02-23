dec46bfc37946cae9dd2aa20a136a6c5cedee780
Redyf <mateusalvespereira7@gmail.com>
2024-02-23 15:01:45 -0300

    feat: Change which key separator

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,14 +1,74 @@
 return {
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-		end,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+	end,
+	opts = {
+		ignore_missing = false,
+		icons = {
+			breadcrumb = "»",
+			separator = "", -- ➜
+			group = "+",
+		},
+		window = {
+			border = "none", -- none, single, double, shadow
+			winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
 		},
 	},
+	-- local wk = require("which-key")
+	-- local ui = require("harpoon.ui")
+	-- local mark = require("harpoon.mark")
+	--
+	-- wk.register({
+	-- 	["<leader>h"] = { name = "󰝆 +harpoon" },
+	-- 	["<C-e>"] = {
+	-- 		function()
+	-- 			ui.toggle_quick_menu()
+	-- 		end,
+	-- 		desc = "Toggle Quick Menu",
+	-- 	},
+	-- 	["<leader>ha"] = {
+	-- 		function()
+	-- 			mark.add_file()
+	-- 		end,
+	-- 		desc = "Add File",
+	-- 	},
+	-- 	["<leader>hj"] = {
+	-- 		function()
+	-- 			ui.nav_file(1)
+	-- 		end,
+	-- 		desc = "Nav File 1",
+	-- 	},
+	-- 	["<leader>hk"] = {
+	-- 		function()
+	-- 			ui.nav_file(2)
+	-- 		end,
+	-- 		desc = "Nav File 2",
+	-- 	},
+	-- 	["<leader>hl"] = {
+	-- 		function()
+	-- 			ui.nav_file(3)
+	-- 		end,
+	-- 		desc = "Nav File 3",
+	-- 	},
+	-- 	["<leader>hm"] = {
+	-- 		function()
+	-- 			ui.nav_file(4)
+	-- 		end,
+	-- 		desc = "Nav File 4",
+	-- 	},
+	-- 	["<leader>hn"] = {
+	-- 		function()
+	-- 			ui.nav_next()
+	-- 		end,
+	-- 		desc = "Nav Next",
+	-- 	},
+	-- 	["<leader>hp"] = {
+	-- 		function()
+	-- 			ui.nav_prev()
+	-- 		end,
+	-- 		desc = "Nav Next",
+	-- 	},
+	-- }),
 }
