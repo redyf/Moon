a08d4a33ebe21401f5fc8f823b969b29c211a7a5
Redyf <mateusalvespereira7@gmail.com>
2024-03-01 20:29:26 -0300

    feat: Add lualine plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,80 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local harpoon = require("harpoon.mark")
+
+			local function truncate_branch_name(branch)
+				if not branch or branch == "" then
+					return ""
+				end
+
+				-- Match the branch name to the specified format
+				local user, team, ticket_number = string.find(branch, "(%w+)%/(%w+)%-(%d+)%-")
+
+				-- If the branch name matches the format, display sko-{ticket_number}, otherwise display the full branch name
+				if ticket_number then
+					return user .. "/" .. team .. "-" .. ticket_number
+				else
+					return branch
+				end
+			end
+
+			local function harpoon_component()
+				local total_marks = harpoon.get_length()
+
+				if total_marks == 0 then
+					return ""
+				end
+
+				local current_mark = "—"
+
+				local mark_idx = harpoon.get_current_index()
+				if mark_idx ~= nil then
+					current_mark = tostring(mark_idx)
+				end
+
+				return string.format("󱡅 %s/%d", current_mark, total_marks)
+			end
+
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "catppuccin",
+					component_separators = { left = "|", right = "|" },
+					section_separators = { left = "█", right = "█" },
+					disabled_filetypes = {
+						statusline = { "dashboard", "alpha", "starter" },
+					},
+					ignore_focus = { "neo-tree" },
+					always_divide_middle = true,
+					globalstatus = true,
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = {
+						{
+							"branch",
+							icon = "",
+							fmt = truncate_branch_name,
+						},
+						harpoon_component,
+						"diff",
+						"diagnostics",
+					},
+					lualine_c = { { "filename", path = 1 } },
+					lualine_x = { "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = {
+						function()
+							return " " .. os.date("%R")
+						end,
+					},
+				},
+				extensions = { "fzf", "lazy" },
+			})
+		end,
+	},
+}
