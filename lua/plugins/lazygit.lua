6718b49b52f3406595db71ec89b818a7ad78b89e
Redyf <mateusalvespereira7@gmail.com>
2024-03-01 20:29:19 -0300

    refactor: Add init function for lazygit keymapping
    If I disable the plugin the keymap will be removed aswell

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,12 +1,13 @@
 return {
 	{
 		"kdheepak/lazygit.nvim",
-		enabled = true,
 		event = "VeryLazy",
 		-- optional for floating window border decoration
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
+		init = function()
+			vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { silent = true, desc = "LazyGit" })
+		end,
 	},
-	vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { silent = true, desc = "LazyGit" }),
 }
