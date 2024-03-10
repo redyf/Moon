68c54339dae6d73323fb71ff08d6298847c475f0
Redyf <mateusalvespereira7@gmail.com>
2024-03-09 23:17:59 -0300

    feat: Disable wrap + add neovide options

--- lua/sets.lua
+++ lua/sets.lua
@@ -20,7 +20,7 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 -- Enable text wrap
-vim.opt.wrap = true
+vim.opt.wrap = false
 
 -- Better splitting
 vim.opt.splitbelow = true
@@ -103,3 +103,28 @@ vim.opt.pumheight = 0
 vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
 
 vim.opt.laststatus = 3 -- (https://neovim.io/doc/user/options.html#'laststatus')
+
+-- Neovide
+local g = vim.g
+local o = vim.o
+if g.neovide then
+	-- Options
+	g.neovide_fullscreen = false
+	g.neovide_hide_mouse_when_typing = false
+	g.neovide_refresh_rate = 165
+	g.neovide_cursor_vfx_mode = "ripple"
+	g.neovide_cursor_animate_command_line = true
+	g.neovide_cursor_animate_in_insert_mode = true
+	g.neovide_cursor_vfx_particle_lifetime = 5.0
+	g.neovide_cursor_vfx_particle_density = 14.0
+	g.neovide_cursor_vfx_particle_speed = 12.0
+	g.neovide_transparency = 0.8
+	--
+	--	-- Fonts
+	o.guifont = "MonoLisa:Medium:h13"
+	-- o.guifont = "JetBrainsMono Nerd Font:h14:Medium:i"
+	-- o.guifont = "FiraMono Nerd Font:Medium:h14"
+	-- o.guifont = "CaskaydiaCove Nerd Font:h14:b:i"
+	-- o.guifont = "BlexMono Nerd Font Mono:h14:Medium:i"
+	-- o.guifont = "Liga SFMono Nerd Font:b:h15"
+end
