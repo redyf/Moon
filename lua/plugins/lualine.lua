b0f0f9f3cde8befb64ff73e00e3182f90331465a
Redyf <mateusalvespereira7@gmail.com>
2024-03-10 14:37:10 -0300

    feat: Enable auto theme for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -42,7 +42,7 @@ return {
 			require("lualine").setup({
 				options = {
 					icons_enabled = true,
-					theme = "catppuccin",
+					theme = "auto",
 					component_separators = { left = "|", right = "|" },
 					section_separators = { left = "█", right = "█" },
 					disabled_filetypes = {
