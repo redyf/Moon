93fb40b09bd1268110a5753c7cefd0bcbfd8d548
Redyf <mateusalvespereira7@gmail.com>
2024-04-30 23:14:51 -0300

    chore: uncomment mappings for lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -242,30 +242,30 @@ return {
 				update_in_insert = true,
 			})
 
-			-- vim.api.nvim_create_autocmd("LspAttach", {
-			-- 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			-- 	callback = function(ev)
-			-- 		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-			--
-			-- 		local opts = { buffer = ev.buf }
-			-- 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			-- 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			-- 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-			-- 		vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
-			-- 		vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
-			-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			-- 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-			-- 		vim.keymap.set("n", "<space>cw", vim.lsp.buf.workspace_symbol, opts)
-			-- 		vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, opts)
-			-- 		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-			-- 		vim.keymap.set("n", "<space>cf", function()
-			-- 			vim.lsp.buf.format({ async = true })
-			-- 		end, opts)
-			-- 		vim.keymap.set("n", "<space>cd", vim.diagnostic.open_float, opts)
-			-- 		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-			-- 		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-			-- 	end,
-			-- })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<space>cw", vim.lsp.buf.workspace_symbol, opts)
+					vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<space>cf", function()
+						vim.lsp.buf.format({ async = true })
+					end, opts)
+					vim.keymap.set("n", "<space>cd", vim.diagnostic.open_float, opts)
+					vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+					vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+				end,
+			})
 		end,
 	},
 }
