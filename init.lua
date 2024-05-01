cc4014d5236558419dd037401621c731b77beb0a
Redyf <mateusalvespereira7@gmail.com>
2024-04-30 23:13:18 -0300

    feat: add option to enable/disable nerd fonts

--- init.lua
+++ init.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = true
 
 -- These must be here for it to package with Nix.
 require("sets")
