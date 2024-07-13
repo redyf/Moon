c2fa0006f2eaf86e715dbb2c01471012798cd2d4
Redyf <mateusalvespereira7@gmail.com>
2024-07-12 21:43:36 -0300

    feat: add cpplint for C

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,6 +5,7 @@ return {
 		config = function()
 			local lint = require("lint")
 			lint.linters_by_ft = {
+				c = { "cpplint" },
 				go = { "golangci-lint" },
 				nix = { "statix" },
 				lua = { "selene" },
