641d86b3388ddaa7f3e6cb2fb7e3c48e5ab3da15
Redyf <mateusalvespereira7@gmail.com>
2024-07-14 19:44:59 -0300

    refactor: unify shell with flake

--- flake.nix
+++ flake.nix
@@ -65,9 +65,16 @@
         };
 
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.stylua
+          nativeBuildInputs = with pkgs; [
+            lua # Lua programming language
+            stylua # Lua formatter
+            selene # Lua linter written in rust
+            lua-language-server # Lua LSP
+            luajitPackages.luarocks-nix # Package manager for Lua on Nix
           ];
+          shellHook = ''
+            echo "Environment is ready" | ${pkgs.lolcat}/bin/lolcat;
+          '';
         };
       }
     );
