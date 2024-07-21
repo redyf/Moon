be853eaed66681cb05e35bc97b0017d364674b70
Redyf <mateusalvespereira7@gmail.com>
2024-07-21 20:59:00 -0300

    style: format properly

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,8 +1,8 @@
 return {
-{
-  "folke/todo-comments.nvim",
-  event = "VeryLazy",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {}
-}
+	{
+		"folke/todo-comments.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
 }
