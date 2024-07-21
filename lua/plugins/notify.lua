42313e94b8f9587967550f1e4b497567073006ff
Redyf <mateusalvespereira7@gmail.com>
2024-07-21 20:58:21 -0300

    feat: toggle format on buffer and nixpkgs
    Disable autoformat on save automatically when on nixpkgs directory, also
    add option to toggle buffer globally or locally.

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -13,7 +13,6 @@ return {
 			})
 			local notify = require("notify")
 
-			-- Enable notifications for toggle commands
 			local function show_notification(message, level)
 				notify(message, level, { title = "conform.nvim" })
 			end
@@ -61,14 +60,37 @@ return {
 				end
 			end
 
+			vim.api.nvim_create_autocmd("BufReadPost", {
+				callback = function()
+					local current_dir = vim.fn.getcwd()
+					local is_nixpkgs = current_dir:match("nixpkgs$")
+					if is_nixpkgs then
+						vim.b.disable_autoformat = true
+						show_notification("Autoformat-on-save disabled for nixpkgs", "info")
+					else
+						vim.b.disable_autoformat = false
+					end
+				end,
+			})
+
 			vim.api.nvim_create_user_command("FormatToggle", function(args)
 				local is_global = not args.bang
+				local current_dir = vim.fn.getcwd()
+				local is_nixpkgs = current_dir:match("nixpkgs$")
+
 				if is_global then
 					vim.g.disable_autoformat = not vim.g.disable_autoformat
 					if vim.g.disable_autoformat then
 						show_notification("Autoformat-on-save disabled globally", "info")
 					else
 						show_notification("Autoformat-on-save enabled globally", "info")
+					end
+				elseif is_nixpkgs then
+					vim.b.disable_autoformat = not vim.b.disable_autoformat
+					if vim.b.disable_autoformat then
+						show_notification("Autoformat-on-save disabled for nixpkgs", "info")
+					else
+						show_notification("Autoformat-on-save enabled for nixpkgs", "info")
 					end
 				else
 					vim.b.disable_autoformat = not vim.b.disable_autoformat
