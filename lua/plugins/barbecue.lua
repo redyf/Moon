70e021a5fa8683c92a814f5c54ea9e11664a08b6
redyf <mateusalvespereira7@gmail.com>
2024-07-23 00:21:14 -0300

    chore: use barbecue on lualine
    I need to clean this file a little bit...

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -14,20 +14,20 @@ return {
 				theme = "auto",
 			})
 
-			vim.api.nvim_create_autocmd({
-				"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
-				"BufWinEnter",
-				"CursorHold",
-				"InsertLeave",
-
-				-- include this if you have set `show_modified` to `true`
-				"BufModifiedSet",
-			}, {
-				group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-				callback = function()
-					require("barbecue.ui").update()
-				end,
-			})
+			-- vim.api.nvim_create_autocmd({
+			-- 	"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+			-- 	"BufWinEnter",
+			-- 	"CursorHold",
+			-- 	"InsertLeave",
+			--
+			-- 	-- include this if you have set `show_modified` to `true`
+			-- 	"BufModifiedSet",
+			-- }, {
+			-- 	group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+			-- 	callback = function()
+			-- 		require("barbecue.ui").update()
+			-- 	end,
+			-- })
 		end,
 	},
 }
