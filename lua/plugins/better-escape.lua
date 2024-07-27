ba7391ffde64b9d77391ac3549e3f36e9449cc67
redyf <mateusalvespereira7@gmail.com>
2024-07-27 19:38:56 -0300

    fix: better-escape being applied to lazygit and terminal
    This was really annoying, but it's fixed now.

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -5,6 +5,31 @@ return {
 		config = function()
 			require("better_escape").setup({
 				timeout = 200,
+				default_mappings = false,
+				mappings = {
+					i = {
+						j = {
+							k = "<Esc>",
+							j = "<Esc>",
+						},
+					},
+					c = {
+						j = {
+							k = "<Esc>",
+							j = "<Esc>",
+						},
+					},
+					v = {
+						j = {
+							k = "<Esc>",
+						},
+					},
+					s = {
+						j = {
+							k = "<Esc>",
+						},
+					},
+				},
 			})
 		end,
 	},
