869c24b97423d1d08d79b4a3b7376006147fca6d
redyf <mateusalvespereira7@gmail.com>
2024-07-29 21:51:08 -0300

    refactor: move mappings to config function
    By doing this I can make sure those mappings will be free if the plugin
    is disabled, instead of enabling them globally which doesn't make sense
    at all.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,10 @@
+dofile(vim.g.base46_cache .. "bufferline")
+
 return {
 	{
 		"akinsho/bufferline.nvim",
 		version = "*",
+		enabled = true,
 		event = "VeryLazy",
 		dependencies = "nvim-tree/nvim-web-devicons",
 		config = function()
@@ -16,24 +19,24 @@ return {
 					-- highlights = catppuccin_hl,
 				},
 			})
+			vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Cycle to Next Buffer" })
+			vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Cycle to previous buffer" })
+			vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Cycle to next buffer" })
+			vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Cycle to previous buffer" })
+			vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
+			vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+			-- Uncomment the block below if needed
+			-- vim.keymap.set("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+			vim.keymap.set("n", "<leader>br", "<cmd>BufferLineCloseRight<cr>", { desc = "Delete buffers to the right" })
+			vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", { desc = "Delete buffers to the left" })
+			vim.keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", { desc = "Delete other buffers" })
+			vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { desc = "Toggle pin" })
+			vim.keymap.set(
+				"n",
+				"<leader>bP",
+				"<Cmd>BufferLineGroupClose ungrouped<CR>",
+				{ desc = "Delete non-pinned buffers" }
+			)
 		end,
 	},
-	vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Cycle to Next Buffer" }),
-	vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Cycle to previous buffer" }),
-	vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Cycle to next buffer" }),
-	vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Cycle to previous buffer" }),
-	vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" }),
-	vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" }),
-	-- Uncomment the block below if needed
-	-- vim.keymap.set("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-	vim.keymap.set("n", "<leader>br", "<cmd>BufferLineCloseRight<cr>", { desc = "Delete buffers to the right" }),
-	vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", { desc = "Delete buffers to the left" }),
-	vim.keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", { desc = "Delete other buffers" }),
-	vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { desc = "Toggle pin" }),
-	vim.keymap.set(
-		"n",
-		"<leader>bP",
-		"<Cmd>BufferLineGroupClose ungrouped<CR>",
-		{ desc = "Delete non-pinned buffers" }
-	),
 }
