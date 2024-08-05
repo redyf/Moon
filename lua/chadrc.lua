747077a7930328f9ff2143301529ff635da0a128
redyf <mateusalvespereira7@gmail.com>
2024-08-05 17:21:03 -0300

    chore: switch statusline theme to minimal

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,10 +15,10 @@ M.ui = {
 	telescope = { style = "bordered" }, -- borderless / bordered
 
 	statusline = {
-		theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+		theme = "minimal", -- default/vscode/vscode_colored/minimal
 		-- default/round/block/arrow separators work only for default statusline theme
 		-- round and block will work for minimal theme only
-		separator_style = "default",
+		separator_style = "round",
 		order = nil,
 		modules = nil,
 	},
