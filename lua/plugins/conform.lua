1a7ea794a98015bfcd0469134f3e667df262a865
redyf <mateusalvespereira7@gmail.com>
2024-10-13 23:11:59 -0300

    refactor: wrap keymaps in init function

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,52 +2,63 @@ return {
 	{
 		"stevearc/conform.nvim",
 		event = "VeryLazy",
-		config = function()
-			local conform = require("conform")
-			conform.setup({
-				format_on_save = function(bufnr)
-					-- Disable with a global or buffer-local variable
-					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-						return
-					end
-					return { timeout_ms = 500, lsp_fallback = true }
-				end,
-				notify_on_error = true,
-				lsp_fallback = true,
-				formatters_by_ft = {
-					["html"] = { { "prettierd", "prettier" } },
-					["css"] = { { "prettierd", "prettier" } },
-					["c"] = { "clang-format" },
-					["cpp"] = { "clang-format" },
-					["go"] = { "gofumpt" },
-					["java"] = { "google-java-format" },
-					["python"] = { "black" },
-					["lua"] = { "stylua" },
-					["yaml"] = { "prettierd" },
-					["rust"] = { "rustfmt" },
-					["markdown"] = { { "prettierd", "prettier" } },
-					["markdown.mdx"] = { { "prettierd", "prettier" } },
-					["javascript"] = { "prettierd", "prettier" },
-					["javascriptreact"] = { "prettierd", "prettier" },
-					["typescript"] = { "prettierd", "prettier" },
-					["typescriptreact"] = { "prettierd", "prettier" },
-					["json"] = { "jq" },
-					["nix"] = { "nixfmt" },
-					["haskell"] = { "ormolu" },
-					["bash"] = { "beautysh" },
-					["ocaml"] = { "ocamlformat" },
-				},
-			})
+		-- This will provide type hinting with LuaLS
+		---@module "conform"
+		---@type conform.setupOpts
+		opts = {
+			notify_on_error = true,
+			lsp_fallback = true,
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				["html"] = { { "prettierd", "prettier", stop_after_first = true } },
+				["css"] = { { "prettierd", "prettier", stop_after_first = true } },
+				["c"] = { "clang-format" },
+				["cpp"] = { "clang-format" },
+				["go"] = { "gofumpt" },
+				["java"] = { "google-java-format" },
+				["python"] = { "black" },
+				["lua"] = { "stylua" },
+				["yaml"] = { "prettierd" },
+				["rust"] = { "rustfmt" },
+				["markdown"] = { { "prettierd", "prettier", stop_after_first = true } },
+				["markdown.mdx"] = { { "prettierd", "prettier", stop_after_first = true } },
+				["javascript"] = { "prettierd", "prettier", stop_after_first = true },
+				["javascriptreact"] = { "prettierd", "prettier", stop_after_first = true },
+				["typescript"] = { "prettierd", "prettier", stop_after_first = true },
+				["typescriptreact"] = { "prettierd", "prettier", stop_after_first = true },
+				["json"] = { "jq" },
+				["nix"] = { "nixfmt" },
+				["haskell"] = { "ormolu" },
+				["bash"] = { "beautysh" },
+				["ocaml"] = { "ocamlformat" },
+			},
+		},
+		init = function()
+			-- Use conform-nvim for gq formatting. ('formatexpr' is set to vim.lsp.formatexpr(), so you can format lines via gq if the language server supports it)
+			vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+			vim.keymap.set(
+				"n",
+				"<leader>uf",
+				"<cmd>FormatToggle<cr>",
+				{ silent = true, desc = "Toggle Format Globally" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>uF",
+				"<cmd>FormatToggle!<cr>",
+				{ silent = true, desc = "Toggle Format Locally" }
+			)
+
+			vim.keymap.set("n", "<leader>cf", function()
+				require("conform").format()
+			end, { silent = true, desc = "Format Buffer" })
+
+			vim.keymap.set("v", "<leader>cF", function()
+				vim.lsp.buf.format()
+			end, { silent = true, desc = "Format Lines" })
 		end,
 	},
-	vim.keymap.set("n", "<leader>uf", "<cmd>FormatToggle<cr>", { silent = true, desc = "Toggle Format Globally" }),
-	vim.keymap.set("n", "<leader>uF", "<cmd>FormatToggle!<cr>", { silent = true, desc = "Toggle Format Locally" }),
-
-	vim.keymap.set("n", "<leader>cf", function()
-		require("conform").format()
-	end, { silent = true, desc = "Format Buffer" }),
-
-	vim.keymap.set("v", "<leader>cF", function()
-		vim.lsp.buf.format()
-	end, { silent = true, desc = "Format Lines" }),
 }
