58fe7ae2b2155a221c311f43048ab488fdd560fb
redyf <mateusalvespereira7@gmail.com>
2026-04-07 22:13:21 -0300

    refactor(treesitter): remove redundant highlight setup and fix FileType autocmd

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,27 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = "main", 
-        build = ":TSUpdate",
-        lazy = false,
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-
-            configs.setup({
-                auto_install = true, 
-                highlight = {
-                    enable = true,
-                },
-            })
-
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = { "*" },
-                callback = function(args)
-                    local bufnr = args.buf
-                    local ft = vim.bo[bufnr].filetype
-                    
-                    local lang = vim.treesitter.language.get_lang(ft) or ft
-
-                    if pcall(vim.treesitter.start, bufnr, lang) then
-                        vim.wo.foldmethod = "expr"
-                        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                    end
-                end,
-            })
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		lazy = false,
+		config = function()
+			require("nvim-treesitter").setup({
+				auto_install = true,
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local bufnr = args.buf
+					local ft = vim.bo[bufnr].filetype
+					if ft == "" then
+						return
+					end
+					local lang = vim.treesitter.language.get_lang(ft) or ft
+					if pcall(vim.treesitter.start, bufnr, lang) then
+						vim.wo.foldmethod = "expr"
+						vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					end
+				end,
+			})
+		end,
+	},
 }
